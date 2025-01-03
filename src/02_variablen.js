8f2811b8ff4b0fb365f57db3a854883dba84d39b
Alphexa <alexander.pe.mobil@gmail.com>
2025-01-03 09:17:59 +0000

    02 / Deklaration + Wertzuweisung

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -4,3 +4,18 @@
  * Blockkommentar funktionieren mit slash und zwei sternchen
  */
 
+/**Variablen 01*******/
+
+//console.log("Hi!");
+//console.log(Hi);
+
+
+/** Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration
+console.log(firstName) // Ausgabe
+firstName = "Moritz" // Wertzuweisung Assignment
+console.log(firstName) // Ausgabe
+let familyName ="Melone" // Deklaration + Wertzuweisung
+console.log("Hi, ich bin " + firstName + " " + familyName + "!")
+
