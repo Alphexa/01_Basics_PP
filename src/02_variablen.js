e89d008f4d1129d059dae8a6a1416117b06d5b01
Alphexa <alexander.pe.mobil@gmail.com>
2025-01-03 10:09:26 +0000

    02 / Variablen vs Konstanten

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -12,10 +12,43 @@
 
 /** Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration
-console.log(firstName) // Ausgabe
-firstName = "Moritz" // Wertzuweisung Assignment
-console.log(firstName) // Ausgabe
-let familyName ="Melone" // Deklaration + Wertzuweisung
-console.log("Hi, ich bin " + firstName + " " + familyName + "!")
+//let firstName; // Deklaration
+//console.log(firstName) // Ausgabe
+//firstName = "Moritz" // Wertzuweisung Assignment
+//console.log(firstName) // Ausgabe
+//let familyName ="Melone" // Deklaration + Wertzuweisung
+//console.log("Hi, ich bin " + firstName + " " + familyName + "!")
 
+/** Wertzuweisung II + Datentypen
+ * JS ist eine untypisierte Sprache
+ */
+
+//let test; // Deklaration
+//console.log("Datentyp: " + typeof test);
+//console.log("Inhalt: " +test);
+//console.log ("----------------------");
+//test = "Bonjour"  // String
+//console.log("Datentyp: " + typeof test);
+//console.log("Inhalt: " +test);
+//console.log ("----------------------");
+//test = 2;
+//console.log("Datentyp: " + typeof test);
+//console.log("Inhalt: " +test);
+//console.log ("----------------------");
+//test= true;
+//console.log("Datentyp: " + typeof test);
+//console.log("Inhalt: " +test);
+//console.log ("----------------------");
+
+
+/** Variablen vs. Konstanten in JS */
+
+let test; // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello" ; // Überschreiben
+console.log ("Inhalt: " + test) // Ausgabe
+
+// Konstante
+const test2 = "Hi!²";
+test2 = "Hello!"; // Laufzeitfehler!!!
+console.log ("Inhalt: " + test2) // Ausgabe
