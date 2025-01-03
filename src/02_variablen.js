ba03f4c9cd000ea02673cacabd2b5caf59647fc9
Alphexa <alexander.pe.mobil@gmail.com>
2025-01-03 10:11:44 +0000

    02 / Variablen vs Konstanten

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -43,12 +43,12 @@
 
 /** Variablen vs. Konstanten in JS */
 
-let test; // Variable : Deklaration
-test = "hi"; // Wertzuweisung (Initialisierung)
-test = "hello" ; // Überschreiben
-console.log ("Inhalt: " + test) // Ausgabe
+//let test; // Variable : Deklaration
+//test = "hi"; // Wertzuweisung (Initialisierung)
+//test = "hello" ; // Überschreiben
+//console.log ("Inhalt: " + test) // Ausgabe
 
 // Konstante
-const test2 = "Hi!²";
-test2 = "Hello!"; // Laufzeitfehler!!!
-console.log ("Inhalt: " + test2) // Ausgabe
+//const test2 = "Hi!²";
+//test2 = "Hello!"; // Laufzeitfehler!!!
+//console.log ("Inhalt: " + test2) // Ausgabe
