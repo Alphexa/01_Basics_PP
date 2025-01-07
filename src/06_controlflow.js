52f781903a0ccf746a1e5b015ccdd873ab09be86
Alphexa <alexander.pe.mobil@gmail.com>
2025-01-07 11:44:07 +0000

    07 / Codechallenge II 1

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -113,17 +113,17 @@ job = "diver"; // .. taucht im Rhein!
 
 /********  Fallunterscheidung / SWITCH|CASE 2 *******/
 
-const cond =false;  // cond = condition = Bedingung
-const value = 1;  // Zahlenwertbelegung
+//const cond =false;  // cond = condition = Bedingung
+//const value = 1;  // Zahlenwertbelegung
 
-switch (cond) {
-    case (value == 1):
-        console.log("Value hat den Wert 1.");
-        break;
-    case (value == 2):
-        console.log("Value hat den Wert 2.");
-        break;
-    default:
-        console.log("Value hat einen anderen Wert.");
-        break;
-}
+//switch (cond) {
+  //  case (value == 1):
+   //     console.log("Value hat den Wert 1.");
+     //   break;
+    //case (value == 2):
+      //  console.log("Value hat den Wert 2.");
+        //break;
+    //default:
+      //  console.log("Value hat einen anderen Wert.");
+        //break;
+//}
